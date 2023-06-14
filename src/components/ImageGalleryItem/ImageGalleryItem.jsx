07eb41b357c2b9bfdd567aca5b18795f5ae18813
DagmaraSobczak <daga.sobczak@gmail.com>
2023-06-15 00:34:23 +0200

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,34 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import css from './ImageGalleryItem.module.css';
 
-const ImageGalleryItem = ({ images, onSelect }) => {
-  if (!images) {
-    return null;
-  }
+const ImageGalleryItem = ({ image, onSelect }) => {
+  const { webformatURL, largeImageURL, tags } = image;
 
-  return images.map(({ id, webformatURL, largeImageURL, tags }) => (
-    <li className={css.imageGalleryItem} key={id}>
+  return (
+    <li className={css.imageGalleryItem}>
       <img
         className={css.imageGalleryItemImage}
         src={webformatURL}
         alt={tags}
-        onClick={() => {
-          onSelect(largeImageURL);
-        }}
+        onClick={() => onSelect(largeImageURL)}
       />
     </li>
-  ));
+  );
 };
 
 ImageGalleryItem.propTypes = {
-  images: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      webformatURL: PropTypes.string.isRequired,
-      largeImageURL: PropTypes.string.isRequired,
-      tags: PropTypes.string,
-    })
-  ),
+  image: PropTypes.shape({
+    webformatURL: PropTypes.string.isRequired,
+    largeImageURL: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+  }).isRequired,
   onSelect: PropTypes.func.isRequired,
 };
 
